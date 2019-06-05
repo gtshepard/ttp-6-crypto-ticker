a811572bad312e29f0e09f328d6e48b95a6814bb
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-05 09:21:38 -0400

    display currency

---
+++ src/nav.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Nav extends Component {
+
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+        <div className="App">
+          <div className ="App-header">
+            <h2> Cryptocurrency  Ticker</h2>
+          </div>
+        </div>
+    )
+  }
+}
+export default Nav
